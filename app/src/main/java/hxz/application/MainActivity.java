551e722863f9994dce0ee10af3864312e9ad9e4b
hxz <huxzzz@126.com>
2017-08-08 13:46:30 +0800

    update class context test

--- app/src/main/java/hxz/application/MainActivity.java
+++ app/src/main/java/hxz/application/MainActivity.java
@@ -2,12 +2,16 @@ package hxz.application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: ");
     }
 }
